2463a18c55a5e29cfaa1afc60f6e19df4ce90197
daiki0508 <oochani05085@outlook.jp>
2021-05-15 04:50:03 +0900

    Fixed a fatal bug

--- app/src/main/java/com/websarva/wings/android/todoapps/DetailContentActivity.java
+++ app/src/main/java/com/websarva/wings/android/todoapps/DetailContentActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +17,21 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 import static com.websarva.wings.android.todoapps.MainActivity.mAuth;
 import static com.websarva.wings.android.todoapps.MainActivity.mGoogleSignInClient;
@@ -32,11 +43,14 @@ public class DetailContentActivity extends AppCompatActivity {
     protected static String note_str;
     private SaveDataClass sdc;
     protected static Map<String, Object> todo_list;
+    private String[] enc_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_content);
+
+        enc_data = new String[3];
 
         ActionBar actionBar = getSupportActionBar();
         Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
@@ -83,11 +97,14 @@ public class DetailContentActivity extends AppCompatActivity {
         if (title_len > 0 && note_len > 0 && title_len <= 30 && note_len <= 100){
             Date date = new Date();
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault());
+            encrypt(note);
 
             todo_list = new HashMap<>();
             todo_list.put("title",title);
-            todo_list.put("note",note);
+            todo_list.put("note",enc_data[0]);
             todo_list.put("date",sdf.format(date));
+            todo_list.put("iv",enc_data[1]);
+            todo_list.put("key",enc_data[2]);
 
             DetailContentDialogFragment dcdf = new DetailContentDialogFragment(this);
             dcdf.show(getSupportFragmentManager(),"DetailContentDialogFragment");
@@ -102,6 +119,42 @@ public class DetailContentActivity extends AppCompatActivity {
                 toast_str = "メモの最大入力可能文字数葉100文字です";
             }
             Toast.makeText(DetailContentActivity.this,toast_str,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void encrypt(String note){
+        byte[] bytes = new byte[256 / 8];
+        byte[] keys = null;
+        SecretKeySpec key;
+
+        keys = getAlias().getBytes(StandardCharsets.UTF_8);
+
+        for (int i = 0; i < getAlias().length(); i++){
+            if (i >= bytes.length){
+                break;
+            }
+            bytes[i] = keys[i];
+        }
+        key = new SecretKeySpec(bytes,"AES");
+
+        byte[] de = null;
+        byte[] iv;
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE,key);
+            de = cipher.doFinal(note.getBytes(StandardCharsets.UTF_8));
+            iv = cipher.getIV();
+
+            enc_data[0] = Base64.encodeToString(de,Base64.DEFAULT);
+            enc_data[1] = Base64.encodeToString(iv,Base64.DEFAULT);
+            enc_data[2] = Base64.encodeToString(keys,Base64.DEFAULT);
+            Log.d("decrypt_de",enc_data[0]);
+            Log.d("decrypt_iv",enc_data[1]);
+            Log.d("decrypt_key",enc_data[2]);
+            Log.d("test",getAlias());
+            // Log.d("decrypt_alias",getAlias());
+        }catch (NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException | IllegalBlockSizeException | InvalidKeyException e){
+            e.printStackTrace();
         }
     }
 
@@ -128,6 +181,11 @@ public class DetailContentActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private native String getAlias();
+    static {
+        System.loadLibrary("main");
     }
 
     @Override
